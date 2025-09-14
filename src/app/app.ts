717395ccd47c95bfb22377730c92f35a37d4a4bd
cesarcamposc <camposcontreras.cesar@gmail.com>
2025-09-14 08:39:09 -0500

    three commits

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 
+import { RouterLink } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',  
 })
 export class App {
   protected readonly title = signal('hola, estamos en angular!');
